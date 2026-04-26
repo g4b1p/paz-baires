30b5d06b312d04d81ad54bbd648d1ad7a6231222
“gabi” <plataramirez17@gmail.com>
2026-04-26 15:08:42 -0300

    cambios web

--- js/componentes.js
+++ js/componentes.js
@@ -165,7 +165,7 @@ function insertarFooter() {
         <hr class="line-footer" />
         <section class="firma">
           <p>Created by</p>
-          <img src="images/GabCoder.png" alt="" style="margin-bottom: 50px;" />
+          <img src="images/GabCoder.png" alt="" style="margin-bottom: 70px;" />
         </section>
       </div>
 
